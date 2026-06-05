349c141f2c441e9f21d874acf92bd204b287772b
naomygonzalezlopez <naomygonzalezlopez9@gmail.com>
2026-06-05 18:11:18 -0400

    Add files via upload

---
+++ Programa 1 c#.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre = "Naomy";
+        string apellido = "Gonzalez Lopez";
+        int edad = 20;
+        double altura = 1.70;
+
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Apellido: " + apellido);
+        Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("Altura: " + altura + " m");
+
+        Console.WriteLine($"\nHola, mi nombre es {nombre} {apellido}, tengo {edad} años y mido {altura} metros.");
+    }
+}
